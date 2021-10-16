ccf9302c0d4e0e28985106dc09bf7fe29cf5f93d
GustavoGJesus <icarogabriel01@protonmail.com>
2021-10-16 15:48:55 -0300

    Initial commit

---
+++ src/components/CoinList/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+
+export function CoinList(){
+    return(
+        <>
+            <Container>
+                <h1>Hello Guys</h1>
+            </Container>
+        
+        </>
+    )
+}
